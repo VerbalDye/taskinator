38c1eb10fb2344fcd33e2a3918f666603140b746
VerbalDye <dhunts258@gmail.com>
2022-03-06 21:54:01 -0500

    working type and name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,24 @@
 var formEl = document.querySelector("#task-form");
 var taskToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //create list item 
     var listItemEl = document.createElement("li");
-    listItemEl.textContent = "This is a new task.";
-    listItemEl.className= "task-item";
+    listItemEl.className = "task-item";
+
+    //create div to hold task info anmd add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to to list
     taskToDoEl.appendChild(listItemEl);
 };
 
