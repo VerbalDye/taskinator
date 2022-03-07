192d2bc286e42555c4b51a2d302fa974e8754d39
VerbalDye <dhunts258@gmail.com>
2022-03-06 20:55:56 -0500

    adding functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
 buttonEl.addEventListener("click", function() {
     var taskItemEl = document.createElement("li");
-    
+    taskItemEl.textContent = "hello";
+    taskItemEl.className = "task-item";
+    tasksToDoEl.appendChild(taskItemEl);
 });
